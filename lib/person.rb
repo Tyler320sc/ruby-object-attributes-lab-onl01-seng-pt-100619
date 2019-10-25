3dc481707fa206a45b532ca4765ef2e9c79314b4
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-25 02:38:24 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,5 +1,5 @@
 class Person
-  
+  def 
 end 
 
 
@@ -9,3 +9,23 @@ end
 
 
 
+# class Dog
+#   def name=(name)
+#     @name = name  
+#   end 
+  
+#   def name
+#     @name
+#   end 
+  
+#   def breed=(breed)
+#     @breed = breed 
+#   end 
+  
+#   def breed
+#     @breed
+#   end 
+  
+#   fido = Dog.new 
+#   fido.name = "Fido"
+# end 
