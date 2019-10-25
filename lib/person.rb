dcaa841590165d20a009cf89442bb140fb57b839
Tyler Carroll <tylercarroll1611@gmail.com>
2019-10-25 02:37:26 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -9,3 +9,8 @@ end
 
 
 
+# context 'with names' do
+#     describe '#name=' do
+#       it 'writes the name of the dog to an instance variable @name' do
+#         fido = Dog.new
+#         fido.name = "Fido"
